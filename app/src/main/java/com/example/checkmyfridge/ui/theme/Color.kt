0738412540d1746e80bd2209afac5d62ec8623ea
hyunjinpark <hjpark0503@naver.com>
2026-03-27 21:26:27 +0900

    Color 추가, Theme 내 Color 명시

--- app/src/main/java/com/example/checkmyfridge/ui/theme/Color.kt
+++ app/src/main/java/com/example/checkmyfridge/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.checkmyfridge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val blue = Color(0xFFB9DAFF)
+val lightBlue = Color(0xFFE0F0FF)
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val navy = Color(0xFF01162D)
+val deepBlue = Color(0xFF0C3B6C)
+val darkBlue = Color(0xFF243A52)
+val brown = Color(0xFF56433D)
+val lightBrown = Color(0xFFBE967A)
+val ibory = Color(0xFFF7F3ED)
+val lightBeige = Color(0xFFE5DCD3)
+val lightYellow = Color(0xFFF6E7A1)
+val cream = Color(0xFFFFF3BF)
+val yellow = Color(0xFFEFD97A)
+val grey = Color(0xFFB6B6B6)
+val darkGrey = Color(0xFF707070)
+val deepGrey = Color(0xFF262626)
